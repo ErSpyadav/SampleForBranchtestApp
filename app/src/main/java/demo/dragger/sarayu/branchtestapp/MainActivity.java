a1f61d63f9472d1fb92bcc03d9b9b65c31652621
SarayuYadav <sarayu.yadaw@tsb.co.uk>
2020-12-17 11:57:45 +0530

    defect-111 comment added

--- app/src/main/java/demo/dragger/sarayu/branchtestapp/MainActivity.java
+++ app/src/main/java/demo/dragger/sarayu/branchtestapp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("This is comment logs");
+        System.out.println("This Chnage from SarayuBranch-pr1");
     }
 }
